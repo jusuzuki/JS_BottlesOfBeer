cb098726657ba49889d75873b712734dcdce9aec
jusuzuki <jsayumi@gmail.com>
2015-09-20 15:40:37 -0700

    Fixed spacing

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 var bottlesOfBeer = function(number) {
   if (number === 0) {
-    return 'No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
+    return ' No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.';
   } else if (number === 1) {
-    return '1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
+    return ' 1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
   } else if (number === 2) {
-    return '2 bottles of beer on the wall, 2 bottles of beer.  Take one down, pass it around, 1 bottle of beer on the wall.' +  bottlesOfBeer(number - 1);
+    return ' 2 bottles of beer on the wall, 2 bottles of beer.  Take one down, pass it around, 1 bottle of beer on the wall.' +  bottlesOfBeer(number - 1);
   } else {
-    return number + ' bottles of beer on the wall, ' + number + ' bottles of beer. Take one down and pass it around, ' + (number - 1) + ' bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
+    return "\n" + number + ' bottles of beer on the wall, ' + number + ' bottles of beer. Take one down and pass it around, ' + (number - 1) + ' bottles of beer on the wall.' +  bottlesOfBeer(number - 1);
     }
   }
 
 
 $(document).ready(function() {
-  $("form#factorial").submit(function(event) {
+  $("form#bottles_of_beer").submit(function(event) {
     var number = parseInt($("input#number").val());
     if(isNaN(number)) {
       $(".song").text("Can't sing to that!");
